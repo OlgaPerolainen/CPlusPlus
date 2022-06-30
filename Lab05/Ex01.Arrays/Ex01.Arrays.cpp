e5ecf26cfb288ece86fcd776ee3e51975cac9cf1
OlgaPerolainen <95132545+OlgaPerolainen@users.noreply.github.com>
2022-06-30 17:54:34 +0300

    Add files via upload

---
+++ Lab05/Ex01.Arrays/Ex01.Arrays.cpp
@@ -0,0 +1,98 @@
+﻿// Ex01.Arrays.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    const int n = 10;
+    int array[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << "Array[" << i <<"] = ";
+        std::cin >> array[i];
+    }
+
+    int s = 0;
+    int sMinus = 0;
+    int sPlus = 0;
+    int sOddInd = 0;
+    int sEvenInd = 0;
+    int maxNum = array[0];
+    int minNum = array[0];
+
+    for (int i = 0; i < n; i++)
+    {
+        s += array[i];
+
+        if (array[i] < 0) sMinus += array[i];
+        else sPlus += array[i];
+
+        if (i % 2 == 0) sEvenInd += array[i];
+        else sOddInd += array[i];
+
+        if (array[i] > maxNum) maxNum = array[i];
+        if (array[i] < minNum) minNum = array[i];
+    }
+
+
+    int indexMax;
+    int indexMin;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] == maxNum)
+        {
+            indexMax = i;
+        }
+
+        if (array[i] == minNum)
+        {
+            indexMin = i;
+        }
+    }
+
+    int multiply = 1;
+    if (indexMax > indexMin)
+    {
+        for (int i = indexMin; i <= indexMax; i++)
+        {
+            multiply *= array[i];
+        }
+    }
+    else
+    {
+        for (int i = indexMax; i <= indexMin; i++)
+        {
+            multiply *= array[i];
+        }
+    }
+    
+
+    std::cout << "Сумма всех элементов массива: " << s << std::endl;
+    std::cout << "Сумма отрицательных элементов массива: " << sMinus << std::endl;
+    std::cout << "Сумма положительных элементов массива: " << sPlus << std::endl;
+    std::cout << "Сумма элементов массива с нечетными индексами: " << sOddInd << std::endl;
+    std::cout << "Сумма элементов массива с четными индексами: " << sEvenInd << std::endl;
+    std::cout << "Индекс элемента массива с наибольшим значением: " << indexMax << std::endl;
+    std::cout << "Индекс элемента массива с наименьшим значением: " << indexMin << std::endl;
+    std::cout << "Произведение элементов массива, расположенных между максимальным и минимальным элементами: " << multiply << std::endl;
+
+
+    double average = (double)s / n;
+    std::cout << "Среднее значение элементов массива: " << average << std::endl;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ Lab05/Ex03.ArraySortPointer/Ex03.ArraySortPointer.cpp
@@ -0,0 +1,67 @@
+﻿// Ex03.ArraySortPointer.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+void show_array(const int[], const int);
+bool from_min(const int, const int);
+bool from_max(const int, const int);
+void bubble_sort(int[], const int, bool (*compare)(int a, int b));
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    const int N = 10;
+    int my_choose = 0;
+    bool (*from_f[2])(int, int) = { from_min,from_max };
+
+    int A[N] = { 9,8,7,6,1,2,3,5,4,9 };
+    std::cout << "1. Сортировать по возрастанию\n";
+    std::cout << "2. Сортировать по убыванию\n";
+    std::cin >> my_choose;
+    std::cout << "Исходные данные: ";
+    show_array(A, N);
+
+    bubble_sort(A, N, (*from_f[my_choose - 1]));
+
+    show_array(A, N);
+
+    return 0;
+}
+
+void show_array(const int Arr[], const int N)
+{
+    for (int i = 0; i < N; i++)
+        std::cout << Arr[i] << " ";
+    std::cout << "\n";
+}
+bool from_min(const int a, const int b)
+{
+    return a > b;
+}
+bool from_max(const int a, const int b)
+{
+    return a < b;
+}
+
+void bubble_sort(int Arr[], const int N, bool (*compare)(int a, int b))
+{
+    for (int i = 1; i < N; i++)
+    {
+        for (int j = 0; j < N - 1; j++)
+        {
+            if ((*compare)(Arr[j], Arr[j + 1])) std::swap(Arr[j], Arr[j + 1]);
+        }
+    }
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
